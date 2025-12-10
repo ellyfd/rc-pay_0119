a016247455b367682df1dd008f6428d4307ee51c
Elly Cheng <ellyfd@gmail.com>
2025-12-10 14:43:02 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -159,7 +159,7 @@ export default function Home() {
             disabled={members.length === 0}>
 
             <Plus className="w-5 h-5 mr-2" />
-            新增交易
+            新增交易（單筆）
           </Button>
           <Button
             onClick={() => setShowBatchTransaction(true)} className="bg-red-500 text-white px-4 py-2 text-sm font-semibold rounded-[50px] inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow h-14 hover:bg-red-600 col-span-2 md:col-span-1"
