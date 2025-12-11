0365f18006499bff953af42214b948ea763e10fd
Elly Cheng <ellyfd@gmail.com>
2025-12-11 09:12:31 +0000

    成員餘額分類付款

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,7 +2,6 @@ import Home from './pages/Home';
 import MemberDetail from './pages/MemberDetail';
 import FoodOrder from './pages/FoodOrder';
 import ProductManagement from './pages/ProductManagement';
-import MemberManagement from './pages/MemberManagement';
 
 
 export const PAGES = {
@@ -10,7 +9,6 @@ export const PAGES = {
     "MemberDetail": MemberDetail,
     "FoodOrder": FoodOrder,
     "ProductManagement": ProductManagement,
-    "MemberManagement": MemberManagement,
 }
 
 export const pagesConfig = {
